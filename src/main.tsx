2c65c50117c243da3858daef6ffcde0f60cdaf2d
Umutcan-YEL <umutcanyel132@gmail.com>
2024-02-05 19:16:48 +0300

    Apollo Client Added

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { ApolloProvider } from "@apollo/client";
+import client from "./services/ApolloClient.ts";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
